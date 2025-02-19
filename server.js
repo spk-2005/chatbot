6e7770b26c0b3063f8835fb9bdae001cf54dd40e
spk-2005 <prasannasimha5002@gmail.com>
2025-02-19 09:19:35 +0530

    first commit

--- server.js
+++ server.js
@@ -7,8 +7,11 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json());
-
+app.use(cors({
+    origin: 'http://localhost:3001', // Allow frontend to access
+    methods: ['GET', 'POST'], // Allowed methods
+    allowedHeaders: ['Content-Type']
+  }));
 // Routes
 app.use('/api', chatRoutes);
 
